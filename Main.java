4cb73259cd16741602640ac13103648b6efa431c
IrBel <47665970+IrBel@users.noreply.github.com>
2019-02-17 18:42:25 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("    *     *  *    *      *");
+        System.out.println("    *   * *   *  *    *  *");
+        System.out.println("    *  ****   *  *    *  *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("*   *");
+        System.out.println(" ***");
+            }
+}
